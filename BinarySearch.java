75b03d15ad18998dee27c616f3b34a6855a7602f
leorjh <leonardojribas@gmail.com>
2023-10-06 11:14:27 -0300

    fix: variable name

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,16 +1,16 @@
 public class BinarySearch{
-    static int binarySearch(int[] numArray, int num, int begin, int end) {
+    static int binarySearch(int[] arrayNum, int num, int begin, int end) {
         int average = 0;
         if(begin < end){
             average = (begin + end) / 2;
         }
-        if(numArray[average] == num){
+        if(arrayNum[average] == num){
             return average;
         }
-        if(numArray[average] > num){
-            return binarySearch(numArray, num, begin, average-1);
+        if(arrayNum[average] > num){
+            return binarySearch(arrayNum, num, begin, average-1);
         }else{
-            return binarySearch(numArray, num, average+1, end);
+            return binarySearch(arrayNum, num, average+1, end);
         }
     }
 }
